d67994c31b6cf8e4ed0ecfc463b42daf720a6f26
Luice Phillips <luice.phillips@gmail.com>
2017-04-24 11:05:11 +0530

    Added Filter for Navigation Menu Arguments

--- header.php
+++ header.php
@@ -42,7 +42,12 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hellobase' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php
+            $defaults = array(
+                'theme_location' => 'menu-1',
+            );
+            wp_nav_menu( apply_filters( 'hellobase_menu_args', $defaults ) );
+            ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
