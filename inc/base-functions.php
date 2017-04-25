4f198ea236ba3d8743f2cf90a5af47625407b701
Dhara Talaviya <dhara.t@ensightglobal.com>
2017-04-25 09:41:13 +0530

    Add Post Format support

--- inc/base-functions.php
+++ inc/base-functions.php
@@ -49,14 +49,9 @@ function hellobase_setup() {
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+	add_theme_support( 'html5', array('search-form','comment-form', 'comment-list', 'gallery','caption',) );
 
+	add_theme_support( 'post-formats', array( 'gallery', 'image', 'video', ) );
 
 
 	// Add user Role of Sub admin
